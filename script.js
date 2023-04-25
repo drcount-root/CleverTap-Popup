ed602dc01e23906b6d9778dedb78ac17f300688d
drcount-root <about.subhamsahu@gmail.com>
2023-04-25 14:57:05 +0530

    hashed perfectly

--- script.js
+++ script.js
@@ -2,45 +2,53 @@
 
 const openModal = document.getElementById("open-modal");
 const modal = document.getElementById("modal");
-const closeModal = document.getElementsByClassName("close")[0];
+// const closeModal = document.getElementsByClassName("close")[0];
 
-openModal.onclick = function () {
+openModal.onload = function () {
   modal.style.display = "block";
 };
 
-closeModal.onclick = function () {
-  modal.style.display = "none";
-};
+// closeModal.onclick = function () {
+//   modal.style.display = "none";
+// };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 
 // ******* GET Doctor's Educational Data In Array Of Objects Format *******
 
 // track id
-
+// http://127.0.0.1:5500/?trackId=36514167930585464182c7e94fa9
 const trackId = window.location.search.slice(9);
-// const trackId = "856971680175575642571d774180";
+// 36514167930585464182c7e94fa9
 
-console.log(trackId)
-
-const hashKey = 'tVbC5agTaQ5YmXraq';
+const hashKey = "tVbC5agTaQ5YmXraq";
 const rounds = 10;
 
-// const bcrypt = require("./bcrypt");
-// import bcrypt from "./bcrypt";
-// let bcrypt;
+let hashedKey;
 
-// bcrypt.genSalt(rounds, (err, salt) => {
-//     bcrypt.hash(hashKey, salt, (err, hash) => {
-//     console.log(hash);
-//   })
-// })
+function crypt() {
+  const hashKey = "tVbC5agTaQ5YmXraq";
+  const salt = "$2a$08$b0MHMsT3ErLoTRjpjzsCie";
 
-async function fetchDoctorsData() {
+  hashpw(hashKey, salt, (newhash) => {
+    hashedKey = newhash;
+    console.log(hashedKey);
+
+    // set hashedKey into session storage
+    sessionStorage.setItem("hashedKey", hashedKey);
+  });
+}
+
+crypt();
+
+const hass = sessionStorage.getItem("hashedKey");
+console.log(hass);
+
+async function fetchDoctorsData(hass) {
   try {
     const response = await fetch(
       "https://dnestapi.docquity.com/4.3/profile-verification",
@@ -54,8 +62,7 @@ async function fetchDoctorsData() {
           version: "4.3",
           devicetype: "web",
           appversion: "123",
-          hashKey:
-            "$2a$10$FiNqN6Y0akzoheI3MECVgeg/sdfaQyPAdDdJP0dPKeVUW1T/clyzm",
+          hashKey: `${hass}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
@@ -104,7 +111,7 @@ async function fetchDoctorsData() {
 
       if (doctorsData.length >= 1) {
         const html = `
-        
+
         <div class="dividers">
             <div class="edu">
                   <span><img src="./assets/CME Credits.svg" /></span>
@@ -249,7 +256,7 @@ async function fetchDoctorsData() {
   }
 }
 
-fetchDoctorsData();
+fetchDoctorsData(hass);
 
 // ** API POST req depending on button click to verify or deny doctors data **
 
@@ -266,8 +273,7 @@ async function verifyDoctorsData(action_value) {
           ver: "4.3",
           devicetype: "web",
           appversion: "123",
-          hashKey:
-            "$2a$10$FiNqN6Y0akzoheI3MECVgeg/sdfaQyPAdDdJP0dPKeVUW1T/clyzm",
+          hashKey: `${hass}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
@@ -278,8 +284,7 @@ async function verifyDoctorsData(action_value) {
     );
 
     const data = await response.json();
-    // console.log(data.msg);
-    if (data.msg == "Success.") console.log("Profile verified");
+    if (data.msg == "Success.") console.log("Checked");
   } catch (error) {
     console.error(error);
   }
@@ -301,106 +306,3 @@ function yesButtonClicked() {
   console.log(action_value);
   verifyDoctorsData(action_value);
 }
-
-//
-//
-//
-//
-//
-//
-
-// const doctorsData = [
-//   {
-//     "1st Degree": "BACHELOR OF MEDICINE AND BACHELOR OF SURGERY",
-//     "1st Degree Awarding Institute": "UNIVERSITY MALAYA",
-//   },
-//   {
-//     "2nd Degree": "BACHELOR OF MEDICINE",
-//     "2nd Degree Awarding Institute": "UNIVERSITY OF UK",
-//   },
-//   {
-//     "3rd Degree": "BACHELOR OF SURGERY",
-//     "3rd Degree Awarding Institute": "UNIVERSITY OF BANGKOK",
-//   },
-// ];
-
-// const container = document.getElementById("edu_container");
-
-// for (let i = 0; i < doctorsData.length; i++) {
-//   const edu = doctorsData[i];
-
-//   const html = `
-//         <div class="card">
-//           <h2>Degree: ${Object.values(edu)[0]}</h2>
-//           <p>From: ${Object.values(edu)[1]}</p>
-//         </div>
-//       `;
-//   container.innerHTML += html;
-// }
-
-// ************** GET Doctor's Educational Data **************
-
-// const doctorsData = [];
-
-// const doctorsDataFetcher = async (URL) => {
-//   const response = await fetch(URL, {
-//     method: "POST",
-//     headers: {
-//       accept: "*/*",
-//       udid: "1234",
-//       lang: "en",
-//       ver: "4.3",
-//       version: "4.3",
-//       devicetype: "web",
-//       appversion: "123",
-//       hashKey: "$2a$10$FiNqN6Y0akzoheI3MECVgeg/sdfaQyPAdDdJP0dPKeVUW1T/clyzm",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       track_id: "70006168129599464368a7a8cef6",
-//     }),
-//   });
-
-//   const data = await response.json();
-//     // console.log(data.data)
-//   doctorsData.push(data.data);
-// };
-
-// doctorsDataFetcher("https://dnestapi.docquity.com/4.3/profile-verification");
-
-// console.log(doctorsData);
-
-//
-//
-//
-//
-//
-//
-// *************************** Example ***************************
-
-// JavaScript
-// const apiUrl = "https://jsonplaceholder.typicode.com/posts";
-
-// async function fetchPosts() {
-//   try {
-//     const response = await fetch(apiUrl);
-//     const posts = await response.json();
-//     const container = document.getElementById("edu_container");
-
-//     for (let i = 0; i < posts.length; i++) {
-//       const post = posts[i];
-//       const html = `
-//         <div class="card">
-//           <h2>${post.title}</h2>
-//           <p>By ${post.userId}</p>
-//           <p>${post.body}</p>
-//         </div>
-//       `;
-//       container.innerHTML += html;
-//     }
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// fetchPosts();
